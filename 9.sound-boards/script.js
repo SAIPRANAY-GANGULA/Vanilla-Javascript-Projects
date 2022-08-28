75ac086af6200a19b002d85e330aba1017c95900
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2022-08-28 10:44:31 +0530

    sound boards

---
+++ 9.sound-boards/script.js
@@ -0,0 +1,24 @@
+const sounds = document.querySelectorAll("audio");
+let soundIds = [];
+
+sounds.forEach((sound) => soundIds.push(sound.id));
+soundIds.forEach((soundId) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = soundId;
+
+  btn.addEventListener("click", () => {
+    stopPreviousPlays();
+    document.getElementById(btn.innerText).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopPreviousPlays() {
+  soundIds.forEach((soundId) => {
+    const sound = document.getElementById(soundId);
+    sound.pause();
+    sound.currentTime = 0;
+  });
+}
