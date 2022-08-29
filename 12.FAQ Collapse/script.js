0fdee23b93aa5f353cb1d9819d1737bec9fa9cad
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2022-08-29 23:00:05 +0530

    faq collapse

---
+++ 12.FAQ Collapse/script.js
@@ -0,0 +1,16 @@
+const toggleButtons = document.querySelectorAll(".faq-toggle");
+const faqs = document.querySelectorAll(".faq");
+
+
+toggleButtons.forEach((toggleButton, index) => {
+    toggleButton.addEventListener('click', () => {
+        faqs[index].classList.toggle('active');
+    })
+})
+
+// Another Solution
+// toggleButtons.forEach((toggleButton) => {
+//     toggleButton.addEventListener('click', () => {
+//         toggleButton.parentNode.classList.toggle('active');
+//     })
+// })
