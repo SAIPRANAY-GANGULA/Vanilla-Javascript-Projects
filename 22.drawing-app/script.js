911f9ebaa84813293193a50156f78e03637aafc4
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2022-09-10 16:01:35 +0530

    draw-app

--- 22.drawing-app/script.js
+++ 22.drawing-app/script.js
@@ -7,7 +7,7 @@ const clearEl = document.getElementById('clear');
 
 const ctx = canvas.getContext('2d');
 
-let size = 10;
+let size = 2;
 let isPressed = false;
 colorEl.value = 'black';
 let color = colorEl.value;
@@ -62,20 +62,20 @@ function updateSizeOnScreen() {
 }
 
 increaseBtn.addEventListener('click', () => {
-    size += 5;
+    size += 2;
 
-    if (size > 50) {
-        size = 50;
+    if (size > 20) {
+        size = 20;
     }
 
     updateSizeOnScreen();
 });
 
 decreaseBtn.addEventListener('click', () => {
-    size -= 5;
+    size -= 2;
 
-    if (size < 5) {
-        size = 5;
+    if (size < 2) {
+        size = 2;
     }
 
     updateSizeOnScreen();
